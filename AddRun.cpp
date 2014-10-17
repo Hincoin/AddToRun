cb793a074339dc300096d6739e7fef3f52711c46
Hincoin <lucenadeveloper@gmail.com>
2014-10-16 18:27:22 -0700

    Added support for additional swithces and parameters

--- AddRun.cpp
+++ AddRun.cpp
@@ -6,6 +6,7 @@
 #include <memory>
 #include <Windows.h>
 #include <vector>
+#include <fstream>
 #include <iostream>
 
 using namespace std;
@@ -19,12 +20,35 @@ vector<wchar_t> to_LPCWSTR(const char* charArray)
     return w_str;
 }
 
+bool dir_does_exist(const string& dirname)
+{
+	DWORD ftype = GetFileAttributesA(dirname.c_str());
+	if(ftype == INVALID_FILE_ATTRIBUTES)
+		return false;
+	if(ftype & FILE_ATTRIBUTE_DIRECTORY)
+		return true;
+	return false; 
+
+
+}
+
 int main()
 {
+
+	CreateDirectory(to_LPCWSTR("C:\\Program Files (x86)\\AddToRun").data(),NULL); // if it exists, nothing will happen, otherwise the directory is created
+	
 	cout << "Enter the name of the desired run command" << endl;
 	string base_str("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\");
 	string new_cmd;
 	getline(cin,new_cmd);
+
+	//string bat_file(dir_base);
+	string bat_location("C:\\Program\ Files\ (x86)\\AddToRun");
+	bat_location.append("\\").append(new_cmd).append(".bat");
+
+	
+	ofstream bat_f(bat_location);
+	
 	auto run_cmd(base_str);
 	run_cmd.append(new_cmd);
 
@@ -45,7 +69,26 @@ int main()
 		cout << "Enter path of executable" << endl;
 		string exectuable;
 		getline(cin,exectuable);
-		auto w_new = to_LPCWSTR(exectuable.c_str());
+
+		if(exectuable[exectuable.length()-1] != '\"')
+		{
+			exectuable.push_back('\"');
+		}
+		if(exectuable[0] != '\"')
+			exectuable.insert(0,"\"");
+
+		
+		bat_f << "@echo off" << endl;
+		bat_f << "start \"\" " << exectuable << " ";
+		cout << "Enter additional options/switches" << endl;
+		string ops;
+		getline(cin,ops);
+
+		bat_f << ops << endl;
+		bat_f.flush();
+		bat_f.close();
+
+		auto w_new = to_LPCWSTR(bat_location.c_str());
 		if(RegSetValue(key,NULL,REG_SZ,w_new.data(),0) == ERROR_SUCCESS)
 		{
 
